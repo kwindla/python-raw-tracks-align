9ed6ab5583850a4c212a824e69eb38c0ec8bae0c
Kwindla Hultman Kramer <kwindla@gmail.com>
2023-12-25 18:08:36 -0800

    improvement to video output and concat

--- transcode-and-pad.py
+++ transcode-and-pad.py
@@ -28,24 +28,25 @@ def transcode_and_pad_audio(input_file_name, output_file_name, start_time):
 
 
 def transcode_and_pad_video(input_file_name, output_file_name, start_time):
-    # command = f'-y -f lavfi -i color=c=black:s=1280x720:r=30:d={math.floor(start_time*1000)} -i {input_file_name} -filter_complex "[0:v][1:v]concat=n=2:v=1:a=0[outv]" -map "[outv]" {output_file_name}'
-    # ffmpeg has several options for concatenation. but they all have
-    # limitations. here again our files that are almost nearly raw video
-    # frames from the rtp stream cause issues for ffmpeg. the most robust
-    # way forward is to generate an mpeg2 file to pad the beginning of
-    # the video, to transcode the rtp video stream to mpeg2, then to use
-    # ffmpeg's concat demuxer to concatenate the two files.
-    command = f'-y -f lavfi -i "color=c=black:s=1280x720" -t {start_time} -r 30 -b:v 24000 /tmp/tmp-padding.mpg'
+    # we need to pad the beginning of the video file. the most robust way to
+    # do this is to generate a file of padding, then concatenate the padding
+    # video and our output video. ffmpeg has several options for concatenation.
+    # we want to use file-level concatenation, so that we can encode our output
+    # video once. file-level concatenation requires two videos with the same
+    # codec, resolution, and frame rate in a container that's friendly to
+    # this file-level concatenation operation.
+    # let's do the following
+    #  1. generate an mp4 file of padding, in a .ts container
+    #  2. generate an mp4 file of the video, in a .ts container
+    #  3. concatenate the two files, copying the video stream into an mp4 container
+    # todo: make our tmp file names opaque so this script can be run in parallel
+    # todo: check to see that output_file_name ends in '.mp4'
+    command = f'-y -f lavfi -i "color=c=black:s=1280x720" -t {start_time} -r 30 -b:v 5000k -c:v libx264 /tmp/padding.ts'
     ffmpegio.ffmpeg(command)
-    command = f'-y -i {input_file_name} -r 30 -b:v 24000 /tmp/tmp-video.mpg'
+    # todo: make the '-vf scale=1280:720' configurable
+    command = f'-y -i {input_file_name} -r 30 -vf scale=1280:720 -b:v 5000k -c:v libx264 /tmp/video.ts'
     ffmpegio.ffmpeg(command)
-    ffconcat = ffmpegio.FFConcat()
-    ffconcat.add_file("/tmp/tmp-padding.mpg")
-    ffconcat.add_file("/tmp/tmp-video.mpg")
-    with ffconcat:
-        ffmpegio.transcode(ffconcat, '/tmp/tmp-concatenated.mpg',
-                           f_in='concat', codec='copy', safe_in=0, overwrite=True)
-    command = f'-y -i /tmp/tmp-concatenated.mpg -c:v libx264 -preset slow -crf 10 {output_file_name}'
+    command = f'-i "concat:/tmp/padding.ts|/tmp/video.ts" -c copy {output_file_name}'
     ffmpegio.ffmpeg(command)
 
 
